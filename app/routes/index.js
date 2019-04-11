c79dca42059d89ce3be4704b805523d7d96dba21
wei.li <274635143@qq.com>
2019-04-11 12:09:33 +0800

    地址改为/yun1/

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,21 +15,21 @@ const {index} = require('../controllers/index');
 const {fzf} = require('../controllers/fzf');
 
 // test
-router.get('/yun/test/get', get);
-router.post('/yun/test/post', post);
+router.get('/yun1/test/get', get);
+router.post('/yun1/test/post', post);
 // login
-router.get('/yun/login/info', checkToken, info);
-router.post('/yun/login/login', checkCode, parameter, login);
-router.post('/yun/login/register', checkCode, parameter, register);
+router.get('/yun1/login/info', checkToken, info);
+router.post('/yun1/login/login', checkCode, parameter, login);
+router.post('/yun1/login/register', checkCode, parameter, register);
 // file
-router.get('/yun/file/url', checkToken, url);
-router.post('/yun/file/del', checkToken, del);
-router.post('/yun/file/edit', checkToken, edit);
-router.post('/yun/file/add', checkToken, add);
-router.get('/yun/file/list', checkToken, list);
-router.post('/yun/file/upload', checkToken, upload);
+router.get('/yun1/file/url', checkToken, url);
+router.post('/yun1/file/del', checkToken, del);
+router.post('/yun1/file/edit', checkToken, edit);
+router.post('/yun1/file/add', checkToken, add);
+router.get('/yun1/file/list', checkToken, list);
+router.post('/yun1/file/upload', checkToken, upload);
 // verification
-router.get('/yun/verification/code', parameter, code);
+router.get('/yun1/verification/code', parameter, code);
 // index
 router.get('/', index);
 // fzf
